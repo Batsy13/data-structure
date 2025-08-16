9531e743f33d7a95e0a8925184c29958196521fb
Pedro Costa <pedrocostavgs@gmail.com>
2025-08-16 17:00:34 -0300

    (python) Add Graph Implementation

---
+++ python/graph.py
@@ -0,0 +1,153 @@
+from abc import ABC, abstractmethod
+
+class Graph(ABC):
+    
+    @abstractmethod
+    def num_of_vertices(self):
+        pass
+    
+    @abstractmethod
+    def num_of_edges(self):
+        pass
+    
+    @abstractmethod
+    def degree_sequence(self):
+        pass
+    
+    @abstractmethod
+    def add_edge(self, u, v):
+        pass
+    
+    @abstractmethod
+    def remove_edge(self, u, v):
+        pass
+    
+    @abstractmethod
+    def print_graph(self):
+        pass
+    
+class DenseGraph(Graph):
+    
+    def __init__(self, num_vertices_or_labels):
+        self.vertices_map = {}
+        self.indices_map = {}
+        self.number_edges = 0
+        
+        if isinstance(num_vertices_or_labels, int):
+            
+            self.num_vertices = num_vertices_or_labels
+            
+            for i in range(self.num_vertices):
+                self.vertices_map[i] = i
+                self.indices_map[i] = i  
+        elif isinstance(num_vertices_or_labels, list):
+            
+            self.num_vertices = len(num_vertices_or_labels)
+            
+            for i, label in enumerate(num_vertices_or_labels):
+                
+                if label in self.vertices_map:
+                    raise ValueError(f"Duplicated label '{label}' in the vertex list")
+                
+                self.vertices_map[label] = i
+                self.indices_map[i] = label
+            
+        else:
+            raise ValueError("Argumento must be an integer number of vertices or a list of labels")
+        
+        self.adjacency_matrix = [[0 for _ in range(self.num_vertices)] for _ in range(self.num_vertices)]
+        
+    def num_of_vertices(self):
+        return self.num_vertices
+    
+    def num_of_edges(self):
+        return self.number_edges
+    
+    def degree_sequence(self):
+        degrees = {}
+        
+        for i in range(self.num_vertices):
+            vertex_label = self.indices_map[i]
+            degree = 0
+            
+            for j in range(self.num_vertices):
+                if self.adjacency_matrix[i][j] == 1:
+                    degree += 1
+            
+            if self.adjacency_matrix[i][i] == 1:
+                degree += 1
+            
+            degrees[vertex_label] = degree
+            
+        return list(degrees.values())
+        
+    def _get_index(self, label):
+        if label not in self.vertices_map:
+            raise ValueError(f"Vertex '{label}' does not exist on graph") 
+        
+        return self.vertices_map[label]
+        
+    def add_edge(self, u, v):
+        try:
+            idx_u = self._get_index(u)
+            idx_v = self._get_index(v)
+        except:
+            print(f"Could not add Edge ({u}, {v})")
+            return
+        
+        if self.adjacency_matrix[idx_u][idx_v] == 0:
+            print(f"Vertex added between {u} and {v}")
+            self.adjacency_matrix[idx_u][idx_v] = 1
+            if u != v:
+                self.adjacency_matrix[idx_v][idx_u] = 1
+            self.number_edges += 1
+                
+        
+    def remove_edge(self, u, v):
+        try:
+            idx_u = self._get_index(u)
+            idx_v = self._get_index(v)
+        except:
+            print(f"Could not remove Edge ({u}, {v})")
+            
+        if self.adjacency_matrix[idx_u][idx_v] == 1:
+            print(f"Removing edge between {u} and {v}")
+            self.adjacency_matrix[idx_u][idx_v] = 0
+            if u != v:
+                self.adjacency_matrix[idx_v][idx_u] = 0
+            self.number_edges -= 0
+        
+    def print_graph(self):
+        if self.num_of_edges == 0:
+            print("Graph is Empty")
+            return
+        
+        print("\nGraph Representation (Adjacency Matrix):")
+        header = "      " + " ".join(f"{self.indices_map[i]:^3}" for i in range(self.num_vertices))
+        print(header)
+        print("    " + "-" * (len(header) - 3))
+        
+        for i in range(self.num_vertices):
+            vertex_label = self.indices_map[i]
+            row_values = " ".join(f"{self.adjacency_matrix[i][j]:^3}" for j in range(self.num_vertices))
+            print(f"{vertex_label:^3} | {row_values}")
+    
+graph = DenseGraph(["A","B","C","D","E"])
+
+print("\n")
+graph.add_edge("A", "B")
+graph.add_edge("A", "C")
+graph.add_edge("C", "D")
+graph.add_edge("C", "E")
+graph.add_edge("B", "D")
+
+graph.print_graph()
+print("\n")
+print(f"Number of vertices: {graph.num_of_vertices()}")
+print(f"Number of edges: {graph.num_of_edges()}")
+print(f"Degrees Sequence: {graph.degree_sequence()}")
+
+graph.remove_edge("A", "C")
+
+graph.print_graph()
+
