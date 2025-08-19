2e02d78ed6a26c32156405e26707d206733fff5a
Batsy13 <pedrocostavgs@gmail.com>
2025-08-18 21:05:51 -0300

    Add SparseGraph Implementation

--- python/graph.py
+++ python/graph.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from collections import defaultdict
 
 class Graph(ABC):
     
@@ -80,6 +81,9 @@ class DenseGraph(Graph):
             degrees[vertex_label] = degree
             
         return list(degrees.values())
+    
+    def degree_sequence2(self):
+        return sorted([sum(row) for row in self.adjacency_matrix])
         
     def _get_index(self, label):
         if label not in self.vertices_map:
@@ -132,22 +136,122 @@ class DenseGraph(Graph):
             row_values = " ".join(f"{self.adjacency_matrix[i][j]:^3}" for j in range(self.num_vertices))
             print(f"{vertex_label:^3} | {row_values}")
     
-graph = DenseGraph(["A","B","C","D","E"])
+# graph = DenseGraph(["A","B","C","D","E"])
 
-print("\n")
-graph.add_edge("A", "B")
-graph.add_edge("A", "C")
-graph.add_edge("C", "D")
-graph.add_edge("C", "E")
-graph.add_edge("B", "D")
+# print("\n")
+# graph.add_edge("A", "B")
+# graph.add_edge("A", "C")
+# graph.add_edge("C", "D")
+# graph.add_edge("C", "E")
+# graph.add_edge("B", "D")
 
-graph.print_graph()
-print("\n")
-print(f"Number of vertices: {graph.num_of_vertices()}")
-print(f"Number of edges: {graph.num_of_edges()}")
-print(f"Degrees Sequence: {graph.degree_sequence()}")
+# graph.print_graph()
+# print("\n")
+# print(f"Number of vertices: {graph.num_of_vertices()}")
+# print(f"Number of edges: {graph.num_of_edges()}")
+# print(f"Degrees Sequence: {graph.degree_sequence()}")
+# print(f"Degrees Sequence: {graph.degree_sequence2()}")
 
-graph.remove_edge("A", "C")
+# graph.remove_edge("A", "C")
 
-graph.print_graph()
+# graph.print_graph()
 
+class SparseGraph(Graph):
+    def __init__(self, num_vertices_or_labels):
+        self.vertices_map = {}
+        self.indices_map = {}
+        self.number_edges = 0
+        
+        if isinstance(num_vertices_or_labels, int):
+            
+            self.num_vertices = num_vertices_or_labels
+            
+            for i in range(self.num_vertices):
+                self.vertices_map[i] = i
+                self.indices_map[i] = i  
+        elif isinstance(num_vertices_or_labels, list):
+            
+            self.num_vertices = len(num_vertices_or_labels)
+            
+            for i, label in enumerate(num_vertices_or_labels):
+                
+                if label in self.vertices_map:
+                    raise ValueError(f"Duplicated label '{label}' in the vertex list")
+                
+                self.vertices_map[label] = i
+                self.indices_map[i] = label
+            
+        else:
+            raise ValueError("Argumento must be an integer number of vertices or a list of labels")
+        
+        self.adjacency_list = defaultdict(list) 
+        
+    def num_of_vertices(self):
+        return self.num_vertices
+    
+    def num_of_edges(self):
+        return self.number_edges
+    
+    def degree_sequence(self):
+        return sorted([len(self.adjacency_list[row]) for row in self.adjacency_list])
+        
+    def _get_index(self, label):
+        if label not in self.vertices_map:
+            raise ValueError(f"Vertex '{label}' does not exist on graph") 
+        
+        return self.vertices_map[label]
+        
+    def add_edge(self, u, v):
+        try:
+            self._get_index(u)
+            self._get_index(v)
+        except:
+            print(f"Could not add Edge ({u}, {v})")
+            return
+        
+        self.adjacency_list[u].append(v)
+        self.adjacency_list[v].append(u)
+        self.number_edges += 1
+        print(f"Edge added between {u} and {v}")
+                
+        
+    def remove_edge(self, u, v):
+        try:
+            self._get_index(u)
+            self._get_index(v)
+        except:
+            print(f"Could not remove Edge ({u}, {v})")
+            
+        print(f"Removing edge between {u} and {v}")
+        self.adjacency_list[u].remove(v)
+        self.number_edges -= 1
+        
+    def print_graph(self):
+        if self.num_of_edges == 0:
+            print("Graph is Empty")
+            return
+        
+        print("\nGraph Representation (Adjacency List):")
+        for vertex, neighbors in self.adjacency_list.items():
+            print(f"{vertex} -> {" ".join(map(str,neighbors))}")
+
+
+graph2 = SparseGraph(["A","B","C","D","E"])
+
+graph2.add_edge("A","B")
+graph2.add_edge("A","C")
+graph2.add_edge("A","C")
+graph2.add_edge("C","D")
+graph2.add_edge("C","E")
+graph2.add_edge("B","D")
+
+graph2.print_graph()
+
+print(f"Number of vertices: {graph2.num_of_vertices()}")
+print(f"Number of edges: {graph2.num_of_edges()}")
+print(f"Degrees Sequence: {graph2.degree_sequence()}")
+
+graph2.remove_edge("A","C")
+graph2.remove_edge("C","A")
+
+graph2.print_graph()
